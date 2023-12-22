019cac78e49a5d395712b7766457919b49f281f7
Nicolas Brunie <nibrunie@gmail.com>
2023-12-22 13:50:28 +0100

    Adding other matrix_tranpose variants

--- src/matrix_transpose/bench_matrix_transpose.c
+++ src/matrix_transpose/bench_matrix_transpose.c
@@ -1,6 +1,20 @@
 // file: bench_matrix_transpose.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+
+/** Display the content of a n x matrix on stdout */
+void matrix_dump(float *mat, unsigned n)
+{
+    size_t i, j;
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n; ++j) printf(" %.3f ", mat[i * n + j]);
+        printf("\n");
+
+    }
+
+}
 
 /** transpose of a n x n matrix
  *
@@ -11,6 +25,17 @@
 void matrix_transpose(float *dst,
                       float *src,
                       size_t n);
+
+void matrix_transpose_intrinsics_4x4(float *dst,
+                                     float *src);
+
+void matrix_transpose_intrinsics(float *dst,
+                                 float *src,
+                                 size_t n);
+
+void matrix_transpose_intrinsics_loads(float *dst,
+                                       float *src,
+                                       size_t n); 
 
 /** return the value of the instret counter
  *
@@ -26,11 +51,14 @@ unsigned long read_instret(void)
 // Defining a default size fot the inputs and output array
 // (can be overloaded during compilation with -DARRAY_SIZE=<value>)
 #ifndef MATRIX_SIZE
-#define MATRIX_SIZE 16
+#define MATRIX_SIZE 4
 #endif
 
 float src[MATRIX_SIZE * MATRIX_SIZE];
 float dst[MATRIX_SIZE * MATRIX_SIZE] = {0.f};
+float dst2[MATRIX_SIZE * MATRIX_SIZE] = {0.f};
+float dst3[MATRIX_SIZE * MATRIX_SIZE] = {0.f};
+float dst4[MATRIX_SIZE * MATRIX_SIZE] = {0.f};
 
 
 int main(void) {
@@ -40,13 +68,48 @@ int main(void) {
         src[i] = rand() / (float) RAND_MAX;
     }
 
+    printf("source matrix:\n");
+    matrix_dump(src, MATRIX_SIZE);
+
     unsigned long start, stop;
     start = read_instret();
     matrix_transpose(dst, src, MATRIX_SIZE);
     stop = read_instret();
 
+    printf("matrix_transpose result:\n");
+    matrix_dump(dst, MATRIX_SIZE);
+
     printf("matrix_transpose used %d instruction(s) to tranpose %dx%d=%d element(s).\n",
            stop - start, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE * MATRIX_SIZE);
 
+    start = read_instret();
+    matrix_transpose_intrinsics_4x4(dst2, src);
+    stop = read_instret();
+
+    printf("matrix_transpose_intrinsics_4x4 result:\n");
+    matrix_dump(dst2, MATRIX_SIZE);
+
+    printf("matrix_transpose_intrinsics_4x4 used %d instruction(s) to tranpose %dx%d=%d element(s).\n",
+           stop - start, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE * MATRIX_SIZE);
+
+    start = read_instret();
+    matrix_transpose_intrinsics(dst3, src, MATRIX_SIZE);
+    stop = read_instret();
+
+    printf("matrix_transpose_intrinsics result:\n");
+    matrix_dump(dst3, MATRIX_SIZE);
+
+    printf("matrix_transpose_intrinsics used %d instruction(s) to tranpose %dx%d=%d element(s).\n",
+           stop - start, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE * MATRIX_SIZE);
+
+    start = read_instret();
+    matrix_transpose_intrinsics_loads(dst4, src, MATRIX_SIZE);
+    stop = read_instret();
+
+    printf("matrix_transpose_intrinsics_loads result:\n");
+    matrix_dump(dst4, MATRIX_SIZE);
+
+    printf("matrix_transpose_intrinsics_loads used %d instruction(s) to tranpose %dx%d=%d element(s).\n",
+           stop - start, MATRIX_SIZE, MATRIX_SIZE, MATRIX_SIZE * MATRIX_SIZE);
     return 0;
 }
--- src/matrix_transpose/matrix_transpose_intrinsics.c
+++ src/matrix_transpose/matrix_transpose_intrinsics.c
@@ -15,3 +15,56 @@ void matrix_transpose(float *dst,
     for (i = 0; i < n; ++i)
         for (j = 0; j < n; ++j) dst[i * n + j] = src[j * n + i];
 };
+
+/** 4x4 matrix transpose using strided stores */
+void matrix_transpose_intrinsics_4x4(float *dst,
+                                     float *src) 
+{
+    unsigned i;
+    for (i = 0; i < 4; ++i) {
+        vfloat32m1_t row = __riscv_vle32_v_f32m1(src + 4 * i, 4);
+        __riscv_vsse32(dst + i, sizeof(float) * 4,row, 4);
+    }
+};
+
+
+/** n x n matrix transpose using strided stores */
+void matrix_transpose_intrinsics(float *dst,
+                                 float *src,
+                                 size_t n) 
+{
+    for (size_t row_id = 0; row_id < n; ++row_id) { // input row-index
+        size_t avl = n;
+        float* row_src = src + row_id * n;
+        float* row_dst = dst + row_id;
+        while (avl > 0) {
+            size_t vl = __riscv_vsetvl_e32m1(avl);
+            vfloat32m1_t row = __riscv_vle32_v_f32m1(row_src, vl);
+            __riscv_vsse32(row_dst, sizeof(float) * n, row, vl);
+            avl -= vl;
+            row_src += vl;
+            row_dst += vl * n;
+        }
+    }
+};
+
+
+/** n x n matrix transpose using strided loads */
+void matrix_transpose_intrinsics_loads(float *dst,
+                                       float *src,
+                                       size_t n) 
+{
+    for (size_t row_id = 0; row_id < n; ++row_id) { // input row-index
+        size_t avl = n;
+        float* col_src = src + row_id;
+        float* row_dst = dst + row_id * n;
+        while (avl > 0) {
+            size_t vl = __riscv_vsetvl_e32m1(avl);
+            vfloat32m1_t row = __riscv_vlse32_v_f32m1(col_src, sizeof(float) * n, vl);
+            __riscv_vse32(row_dst, row, vl);
+            avl -= vl;
+            col_src += vl * n;
+            row_dst += vl;
+        }
+    }
+};
