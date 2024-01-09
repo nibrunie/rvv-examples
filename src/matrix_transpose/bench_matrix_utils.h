d3e8801b29bebd31c318569c11946e5695356781
Nicolas Brunie <nibrunie@gmail.com>
2024-01-09 12:37:02 -0800

    fixing performance metric display

--- src/matrix_transpose/bench_matrix_utils.h
+++ src/matrix_transpose/bench_matrix_utils.h
@@ -14,11 +14,14 @@ static unsigned long read_perf_counter(void)
 {
   unsigned long counter_value;
 #if defined(COUNT_INSTRET)
+#define PERF_METRIC "instruction"
   asm volatile ("rdinstret %0" : "=r" (counter_value));
 #elif defined(COUNT_CYCLE)
+#define PERF_METRIC "cycle"
   asm volatile ("rdcycle %0" : "=r" (counter_value));
 #else
   // instret is also the default
+#define PERF_METRIC "instruction"
   asm volatile ("rdinstret %0" : "=r" (counter_value));
 #endif
   return counter_value;
