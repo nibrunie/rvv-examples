451447a181eb5a87c3a4a4e019b2cff60784a541
Nicolas Brunie <nibrunie@gmail.com>
2024-04-21 11:26:54 -0700

    Adding poly_mult_scalar_opt implementation

--- src/polynomial_mult/poly_mult_baseline.c
+++ src/polynomial_mult/poly_mult_baseline.c
@@ -28,14 +28,31 @@ void poly_mult_baseline(polynomial_t* dst, polynomial_t lhs, polynomial_t rhs)
     }
 }
 
+void poly_mult_scalar_opt(polynomial_t* dst, polynomial_t lhs, polynomial_t rhs)
+{
+    int d;
+    assert(rhs.modulo == lhs.modulo);
+    dst->degree = lhs.degree + rhs.degree;
+    dst->modulo = lhs.modulo;
+    for (d = 0; d <= rhs.degree; ++d) {
+        int k;
+        dst->coeffs[d] = 0;
+        for (k = 0; k <= (d <= lhs.degree ? d : lhs.degree); ++k) dst->coeffs[d] += (lhs.coeffs[k] * rhs.coeffs[d - k]) % lhs.modulo;
+    }
+    for (d = rhs.degree + 1; d <= lhs.degree + rhs.degree; ++d) {
+        int k;
+        dst->coeffs[d] = 0;
+        for (k = d - rhs.degree; k <= (d <= lhs.degree ? d : lhs.degree); ++k) dst->coeffs[d] += (lhs.coeffs[k] * rhs.coeffs[d - k]) % lhs.modulo;
+    }
+}
 
-/** generic benchmark wrapper for n-element 1D softmax implementation
+/** generic benchmark wrapper for polynomial multiplication implementation
  *
- * @param dst destination array (must be at least n-element wide)
- * @param src source array (must be at least n-element wide)
+ * @param dst destination polynomial
+ * @param lhs left hand side input polynomial
+ * @param rhs right hand side input polynomial
  * @param func function under test
- * @param goldem golden value for destination array (must contain n valid elements)
- * @param n number of elements to be considered
+ * @param golden golden value for destination array (must contain n valid elements)
 */
 poly_mult_bench_result_t poly_mult_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, poly_mult_func_t func, polynomial_t* golden) {
     unsigned long start, stop;
@@ -57,4 +74,8 @@ poly_mult_bench_result_t poly_mult_bench(polynomial_t* dst, polynomial_t* lhs, p
 
 poly_mult_bench_result_t poly_mult_baseline_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, polynomial_t* golden) {
     return poly_mult_bench(dst, lhs, rhs, poly_mult_baseline, golden);
+}
+
+poly_mult_bench_result_t poly_mult_scalar_opt_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, polynomial_t* golden) {
+    return poly_mult_bench(dst, lhs, rhs, poly_mult_scalar_opt, golden);
 }
