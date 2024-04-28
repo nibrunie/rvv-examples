9214c991220efc72fda5eafca1cebd3ba6877149
Nicolas Brunie <nibrunie@gmail.com>
2024-04-28 11:26:13 -0700

    prototyping fast NTT in Python

--- src/polynomial_mult/poly_mult_emulation.py
+++ src/polynomial_mult/poly_mult_emulation.py
@@ -9,17 +9,28 @@ class Ring:
         self.invDegree = self.elt(invDegree)
         self.rootOfUnity = self.elt(rootOfUnity)
         self.invRootOfUnity = self.elt(invRootOfUnity)
-        assert (rootOfUnity * invRootOfUnity) % modulo == 1
-        assert (rootOfUnity ** degree) % modulo == 1
-        assert (degree * invDegree) % modulo == 1
+        if not invRootOfUnity is None: assert (rootOfUnity * invRootOfUnity) % modulo == 1
+        assert (self.rootOfUnity ** degree).value % modulo == 1
+        if not invDegree is None: assert (degree * invDegree) % modulo == 1
 
     def elt(self, value):
         if isinstance(value, RingElt):
-            assert value.ring == self
+            # assert value.ring == self
             return value
         if isinstance(value, int):
             return RingElt(value, self)
+        if value is None:
+            return None
+        print(value.__class__)
         raise NotImplementedError
+
+    def powerRing(self):
+        """ sub-ring with half the degreee and the square of the root of unity"""
+        return Ring(self.modulo, self.degree.value // 2, self.rootOfUnity ** 2, None, None)
+
+    def __eq__(lhs, rhs) -> bool:
+        # limiting equality check to modulo, degree and rootOfUnity
+        return lhs.modulo == rhs.modulo and lhs.rootOfUnity == rhs.rootOfUnity and lhs.degree == rhs.degree
 
     @property
     def zero(self):
@@ -55,6 +66,9 @@ class RingElt:
             return self * self**(n-1)
         raise NotImplementedError("power must be positive")
 
+    def __neg__(self):
+        return RingElt(-self.value, self.ring)
+
     def __repr__(self) -> str:
         return f"{self.value}"
 
@@ -65,14 +79,12 @@ class RingElt:
             return self.value == value.value
         raise NotImplementedError
         
-DefaultRing = Ring(19, 3, 11, 7, 13)
 
 class Polynomial:
-    def __init__(self, coeffs, eltRing = DefaultRing):
+    def __init__(self, coeffs, eltRing: Ring):
         self.degree = len(coeffs) - 1
         self.coeffs = [eltRing.elt(v) for v in coeffs]
         self.eltRing = eltRing
-
 
     def __add__(lhs, rhs):
         assert lhs.eltRing == rhs.eltRing
@@ -122,6 +134,14 @@ class Polynomial:
     def isZero(self):
         return all(v == 0 for v in self.coeffs)
 
+    def extractEven(self):
+        """ extract sub-polynomial with even-index coefficients """
+        return Polynomial([self.coeffs[i] for i in range(0, self.degree + 1, 2)], self.eltRing.powerRing())
+
+    def extractOdd(self):
+        """ extract sub-polynomial with odd-index coefficients """
+        return Polynomial([self.coeffs[i] for i in range(1, self.degree + 1, 2)], self.eltRing.powerRing())
+
     @staticmethod
     def monomial(degree, eltRing):
         return Polynomial([0] * degree + [1], eltRing)
@@ -135,7 +155,7 @@ class Polynomial:
         return Polynomial([random.randrange(eltRing.modulo) for d in range(degree + 1)], eltRing)
 
 class NTTDomain:
-    def __init__(self, coeffs, eltRing = DefaultRing):
+    def __init__(self, coeffs, eltRing):
         self.degree = len(coeffs) - 1
         self.coeffs = [eltRing.elt(v) for v in coeffs]
         self.eltRing = eltRing
@@ -161,6 +181,7 @@ class NTTDomain:
 
 
 def ntt_transform(poly: Polynomial) -> NTTDomain:
+    """ Number Theoretic transform on @p poly """
     ntt_coeffs = []
     for k in range(len(poly.coeffs)):
         ntt_coeff = poly.eltRing.elt(0)
@@ -178,16 +199,52 @@ def ntt_inv_transform(ntt: NTTDomain) -> Polynomial:
         poly_coeffs.append(ntt.eltRing.invDegree * poly_coeff)
     return Polynomial(poly_coeffs, ntt.eltRing)
 
+def fast_ntt_transform(poly: Polynomial) -> NTTDomain:
+    """ """
+    if poly.degree == 0:
+        print("fast NTT: level 1 reached")
+        return NTTDomain(poly.coeffs[:1], poly.eltRing)
+    if poly.degree % 2 == 0:
+        print(f"fast NTT: even degree {poly.degree}, fall back to slow NTT")
+        # only works if the polynomial has an even number of coefficients
+        # else fallback to slow method
+        return ntt_transform(poly)
+    else:
+        print("fast NTT: split and reconstruction")
+        poly_even = poly.extractEven()
+        poly_odd = poly.extractOdd()
+        # recursive NTT
+        ntt_even = fast_ntt_transform(poly_even)
+        ntt_odd = fast_ntt_transform(poly_odd)
+        # reconstruction
+        ntt_coeffs = []
+        half_degree = (poly.degree + 1) // 2
+        for i in range(poly.degree + 1):        
+            j = i % half_degree
+            even_coeff = ntt_even.coeffs[j] 
+            odd_coeff = ntt_odd.coeffs[j] 
+            twiddle = poly.eltRing.rootOfUnity ** j 
+            if i >= half_degree:
+                twiddle = -twiddle
+            ntt_coeff = even_coeff + twiddle * odd_coeff
+            ntt_coeffs.append(ntt_coeff)
+        return NTTDomain(ntt_coeffs, poly.eltRing)
+
+
 
 if __name__ == "__main__":
-    print(f"coefficient arithmetic is done in {DefaultRing}")
+    # DefaultRing = Ring(19, 3, 11, 7, 13)
     # declaring basis polynomial
-    mod = Polynomial.monomial(3, DefaultRing) + Polynomial.constant(-1, DefaultRing)
+    # mod = Polynomial.monomial(3, DefaultRing) + Polynomial.constant(-1, DefaultRing)
+    DefaultRing = Ring(3329, 4, (17 ** 64) % 3329, 1600, 2497)
+    mod = Polynomial.monomial(4, DefaultRing) + Polynomial.constant(-1, DefaultRing)
+
+    print(f"coefficient arithmetic is done in {DefaultRing}")
 
     # display of various objects and operations (no self check [yet ?])
 
     # simple polynomials and basic arithmetic on them
-    a = Polynomial([DefaultRing.elt(1), DefaultRing.elt(1)])
+    a = Polynomial([DefaultRing.elt(1), DefaultRing.elt(1)], DefaultRing)
     print(f"a = X + 1 = {a}")
     print(f"a * a = {a * a}")
     print(f"a - a = 0 = {a - a}")
@@ -197,10 +254,13 @@ if __name__ == "__main__":
     print("f X^257 % mod = {Polynomial.monomial(257, DefaultRing) % mod}")
 
     # Random polynomial and basic NTT testing
-    polys = [Polynomial.random(2, DefaultRing) for _ in range(2)]
+    polys = [Polynomial.random(3, DefaultRing) for _ in range(2)]
     print(f"Random polys[:] = {list(str(p) for p in polys)}")
     ntts = [ntt_transform(poly) for poly in polys]
     print(f"polys[0] + polys[1] = {polys[0] + polys[1]} (direct addition)" )
     print(f"polys[0] + polys[1] = {ntt_inv_transform(ntts[0] + ntts[1])} (ntt -> addition -> inv ntt)" )
     print(f"polys[0] * polys[1] % mod = {(polys[0] * polys[1]) % mod} (direct modulo multiplication)" )
     print(f"polys[0] * polys[1] % mod = {ntt_inv_transform(ntts[0] * ntts[1])} (ntt -> multiplication -> inv ntt)" )
+
+    print(f"ntt_transform(polys[0) = {ntts[0].coeffs}")
+    print(f"fast_ntt_transform(polys[0) = {fast_ntt_transform(polys[0]).coeffs}")
