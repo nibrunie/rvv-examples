2efc40db19e942d95cec76ff212acb3683096576
Nicolas Brunie <nibrunie@gmail.com>
2024-05-12 10:27:53 -0700

    Refactoring to integrate modulo as function parameter

--- src/polynomial_mult/poly_mult_baseline.c
+++ src/polynomial_mult/poly_mult_baseline.c
@@ -16,14 +16,17 @@ void poly_mult_baseline(polynomial_t* dst, polynomial_t lhs, polynomial_t rhs)
     int d;
     assert(rhs.modulo == lhs.modulo);
     dst->degree = lhs.degree + rhs.degree;
+    assert(dst->coeffSize >= dst->degree + 1);
     dst->modulo = lhs.modulo;
     for (d = 0; d <= lhs.degree + rhs.degree; ++d) {
         int k;
         dst->coeffs[d] = 0;
         if (d <= rhs.degree) {
             for (k = 0; k <= (d <= lhs.degree ? d : lhs.degree); ++k) dst->coeffs[d] += (lhs.coeffs[k] * rhs.coeffs[d - k]) % lhs.modulo;
+            dst->coeffs[d] %= lhs.modulo;
         } else {
             for (k = d - rhs.degree; k <= (d <= lhs.degree ? d : lhs.degree); ++k) dst->coeffs[d] += (lhs.coeffs[k] * rhs.coeffs[d - k]) % lhs.modulo;
+            dst->coeffs[d] %= lhs.modulo;
         }
     }
 }
@@ -95,6 +98,21 @@ void poly_in_place_mod(polynomial_t* dst, polynomial_t modulo) {
         }
         max_non_zero_src_degree--;
     }
+    dst->degree = modulo.degree - 1;
+}
+
+
+/** Baseline implementation of polynomial multiplication
+ *
+ * @param dst destination polynomial
+ * @param lhs left hand side input polynomial
+ * @param rhs right hand side input polynomial
+*/
+void poly_mult_mod_baseline(polynomial_t* dst, polynomial_t lhs, polynomial_t rhs, polynomial_t modulo)
+{
+    poly_mult_baseline(dst, lhs, rhs);
+
+    poly_in_place_mod(dst, modulo);
 }
 
 
@@ -120,7 +138,7 @@ void poly_mod(polynomial_t* dst, polynomial_t src, polynomial_t modulo)
 }
 
 
-void poly_mult_scalar_opt(polynomial_t* dst, polynomial_t lhs, polynomial_t rhs)
+void poly_mult_scalar_opt(polynomial_t* dst, polynomial_t lhs, polynomial_t rhs, polynomial_t modulo)
 {
     int d;
     assert(rhs.modulo == lhs.modulo);
@@ -142,38 +160,15 @@ void poly_mult_scalar_opt(polynomial_t* dst, polynomial_t lhs, polynomial_t rhs)
         // sum without wrapping around)
         dst->coeffs[d] %= lhs.modulo;
     }
-}
 
-/** generic benchmark wrapper for polynomial multiplication implementation
- *
- * @param dst destination polynomial
- * @param lhs left hand side input polynomial
- * @param rhs right hand side input polynomial
- * @param func function under test
- * @param golden golden value for destination array (must contain n valid elements)
-*/
-poly_mult_bench_result_t poly_mult_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, poly_mult_func_t func, polynomial_t* golden) {
-    unsigned long start, stop;
-    poly_mult_bench_result_t bench_result;
-
-    start = read_perf_counter();
-    func(dst, *lhs, *rhs);
-    stop = read_perf_counter();
-    bench_result.perf_count = stop - start;
-
-    bench_result.errors = compare_poly(*dst, *golden);
-
-#       ifdef VERY_VERBOSE
-        printf("errors: %d\n", bench_result.errors);
-#       endif
-    return bench_result;
+    poly_in_place_mod(dst, modulo);
 }
 
 
-poly_mult_bench_result_t poly_mult_baseline_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, polynomial_t* golden) {
-    return poly_mult_bench(dst, lhs, rhs, poly_mult_baseline, golden);
+poly_mult_bench_result_t poly_mult_mod_baseline_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, polynomial_t* modulo, polynomial_t* golden) {
+    return poly_mult_bench(dst, lhs, rhs, modulo, poly_mult_mod_baseline, golden);
 }
 
-poly_mult_bench_result_t poly_mult_scalar_opt_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, polynomial_t* golden) {
-    return poly_mult_bench(dst, lhs, rhs, poly_mult_scalar_opt, golden);
+poly_mult_bench_result_t poly_mult_mod_scalar_opt_bench(polynomial_t* dst, polynomial_t* lhs, polynomial_t* rhs, polynomial_t* modulo, polynomial_t* golden) {
+    return poly_mult_bench(dst, lhs, rhs, modulo, poly_mult_scalar_opt, golden);
 }
