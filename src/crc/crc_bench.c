3637465d083f624395b64f95aa4f169d7cd03783
Nicolas Brunie <nibrunie@gmail.com>
2024-07-28 10:07:14 -0700

    Cleaning up folding constants init
    * They are now statically copied in function rather than passed as arguments.

--- src/crc/crc_bench.c
+++ src/crc/crc_bench.c
@@ -27,11 +27,16 @@ uint32_t crcEth32_le_vector(uint32_t crc, unsigned char const *p, size_t len);
  *  It supports an arbitrary polynomial through the use of a table of reduction
  *  constants transmitted through a parameter.
  */
-uint32_t crc_be_vector_opt(uint32_t crc, unsigned char const *p, size_t len, uint32_t redCsts[]);
+uint32_t crc_be_vector_opt(uint32_t crc, unsigned char const *p, size_t len);
 
 uint32_t crc_le_vector_opt(uint32_t crc, unsigned char const *p, size_t len);
 
-uint32_t crc_be_vector_zvbc32e(uint32_t crc, unsigned char const *p, size_t len, const uint32_t redCsts[]);
+uint32_t crc_be_vector_zvbc32e(uint32_t crc, unsigned char const *p, size_t len);
+
+typedef struct {
+    uint32_t (*crc_func)(uint32_t seed, unsigned char const* p, size_t len);
+    char* label;
+} crc_bench_t;
 
 /** basic counter read function */
 unsigned long read_cycles(void)
@@ -63,27 +68,6 @@ int bench_crc_be(void)
     printf("CRC32 BE table init in %u cycle(s)\n", stop - start);
 #endif // ifdef VERBOSE
 
-    // generating constant for vector multiplication
-    uint8_t dbgMsg[128] = {0};
-    dbgMsg[0] = 1;
-#ifdef VERY_VERBOSE
-    for (int i = 32; i <= 512; i +=32) {
-        printf("CRC32(X^%d)  = %"PRIx32" (crc32_be_generic)\n", i, crc32_be_generic(0, dbgMsg, 1 + i / 8, ethCRC32Poly));
-    }
-#endif
-
-    start = read_cycles();
-    uint32_t vectorRedConstants[2];
-    vectorRedConstants[0] = crc32_be_generic(0, dbgMsg, 1 + 20, ethCRC32Poly); // X^160 mod polynomial
-    vectorRedConstants[1] = crc32_be_generic(0, dbgMsg, 1 + 12, ethCRC32Poly); // X^96 mod polynomial
-    uint32_t vectorRedConstantsZvbc32e[4];
-    for (int i = 0; i < 4; ++i) vectorRedConstantsZvbc32e[i] = crc32_be_generic(0, dbgMsg, 1 + 16 - 4 * i, ethCRC32Poly);
-    stop = read_cycles();
-
-#ifdef VERBOSE
-    printf("crc_be_vector_opt reduction constant table init in %u cycle(s)\n", stop - start);
-#endif // ifdef VERBOSE
-
 
     size_t msgSize = MSG_SIZES;
     unsigned char *inputMsg = (unsigned char*) malloc(msgSize);
@@ -105,7 +89,7 @@ int bench_crc_be(void)
     float throughputVector = (double) delayVector / msgSize; 
 
     start = read_cycles();
-    uint32_t resVectorOpt = crc_be_vector_opt(0, inputMsg, msgSize, vectorRedConstants);
+    uint32_t resVectorOpt = crc_be_vector_opt(0, inputMsg, msgSize);
     stop = read_cycles();
 
     uint32_t delayVectorOpt = stop - start;
@@ -113,7 +97,7 @@ int bench_crc_be(void)
 
     // FIXME: Zvbc32e needs a specific 4x32-bit vectorRedConstants
     start = read_cycles();
-    uint32_t resVectorZvbc32e = crc_be_vector_zvbc32e(0, inputMsg, msgSize, vectorRedConstantsZvbc32e);
+    uint32_t resVectorZvbc32e = crc_be_vector_zvbc32e(0, inputMsg, msgSize);
     stop = read_cycles();
 
     uint32_t delayVectorZvbc32e = stop - start;
@@ -135,6 +119,51 @@ int bench_crc_be(void)
     return !(resVector == resGeneric && resVectorOpt == resGeneric);
 }
 
+void compute_crc_reduction_constants() {
+    uint32_t start = 0, stop = 0;
+    const uint32_t ethCRC32Poly = 0x04C11DB7;
+    const uint32_t ethCRC32PolyInv = 0xedb88320;
+
+    // generating constant for vector multiplication
+    uint8_t dbgMsg[128] = {0};
+    // LE CRC requires X^MSB to appear as the most significant bit of the first byte
+    dbgMsg[0] = 0x80;
+    for (int i = 32; i <= 128; i +=32) {
+        printf("CRC32(X^%d)  = %"PRIx32" (crc32_le_generic)\n", i, crc32_le_generic(0, dbgMsg, 1 + i / 8, ethCRC32PolyInv));
+    }
+    start = read_cycles();
+    uint32_t vectorRedConstantsLE[2];
+    vectorRedConstantsLE[0] = crc32_le_generic(0, dbgMsg, 1 + 20, ethCRC32Poly); // X^160 mod polynomial
+    vectorRedConstantsLE[1] = crc32_le_generic(0, dbgMsg, 1 + 12, ethCRC32Poly); // X^160 mod polynomial
+    stop = read_cycles();
+    printf("crc_le_vector_opt reduction constant table init in %u cycle(s)\n", stop - start);
+    for (int i = 0; i < 2; ++i) printf("[LE] vectorRedConstants[%d] = %"PRIx32"\n", i, vectorRedConstantsLE[i]);
+
+
+    // BE CRC requires X^MSB to appear as the least significant bit of the first byte
+    dbgMsg[0] = 1;
+    for (int i = 32; i <= 512; i +=32) {
+        printf("CRC32(X^%d)  = %"PRIx32" (crc32_be_generic)\n", i, crc32_be_generic(0, dbgMsg, 1 + i / 8, ethCRC32Poly));
+    }
+
+    start = read_cycles();
+    uint32_t vectorRedConstantsBE[2];
+    vectorRedConstantsBE[0] = crc32_be_generic(0, dbgMsg, 1 + 20, ethCRC32Poly); // X^160 mod polynomial
+    vectorRedConstantsBE[1] = crc32_be_generic(0, dbgMsg, 1 + 12, ethCRC32Poly); // X^96 mod polynomial
+    for (int i = 0; i < 2; ++i) printf("[BE] vectorRedConstants[%d] = %"PRIx32"\n", i, vectorRedConstantsBE[i]);
+
+    uint32_t vectorRedConstantsZvbc32e[4];
+    for (int i = 0; i < 4; ++i) {
+        vectorRedConstantsZvbc32e[i] = crc32_be_generic(0, dbgMsg, 1 + 16 - 4 * i, ethCRC32Poly);
+        printf("[BE] vectorRedConstantsZvbc32e[%d] = %"PRIx32"\n", i, vectorRedConstantsZvbc32e[i]);
+    }
+    stop = read_cycles();
+
+#ifdef VERBOSE
+    printf("crc_be_vector_opt reduction constant table init in %u cycle(s)\n", stop - start);
+#endif // ifdef VERBOSE
+}
+
 int bench_crc_le(void)
 {
     // Normal order, TODO: check if reverse is required
@@ -150,27 +179,6 @@ int bench_crc_le(void)
 #ifdef VERY_VERBOSE
     printf("CRC32 LE table init in %u cycle(s)\n", stop - start);
 #endif
-
-    // generating constant for vector multiplication
-    uint8_t dbgMsg[128] = {0};
-#ifdef VERY_VERBOSE
-    dbgMsg[0] = 1;
-    for (int i = 32; i <= 512; i +=32) {
-        printf("CRC32(X^%d)  = %"PRIx32" (crc32_be_generic)\n", i, crc32_be_generic(0, dbgMsg, 1 + i / 8, ethCRC32Poly));
-    }
-    dbgMsg[0] = 0x80;
-    for (int i = 32; i <= 128; i +=32) {
-        printf("CRC32(X^%d)  = %"PRIx32" (crc32_le_generic)\n", i, crc32_le_generic(0, dbgMsg, 1 + i / 8, ethCRC32PolyInv));
-    }
-#endif
-
-    start = read_cycles();
-    uint32_t vectorRedConstants[2];
-    vectorRedConstants[0] = crc32_le_generic(0, dbgMsg, 1 + 20, ethCRC32Poly); // X^160 mod polynomial
-    vectorRedConstants[1] = crc32_le_generic(0, dbgMsg, 1 + 12, ethCRC32Poly); // X^160 mod polynomial
-    stop = read_cycles();
-    printf("crc_le_vector_opt reduction constant table init in %u cycle(s)\n", stop - start);
-
 
     size_t msgSize = MSG_SIZES;
     unsigned char *inputMsg = (unsigned char*) malloc(msgSize);
@@ -214,5 +222,10 @@ int bench_crc_le(void)
 }
 
 int main(void) {
+    // define to compute and display the static constants (X^i mod P) = CRC(X^(i-32)) used
+    // in folding implementations
+#ifdef DISPLAY_REDUCTION_CSTS
+    compute_crc_reduction_constants();
+#endif
     return bench_crc_be() || bench_crc_le();
 }
