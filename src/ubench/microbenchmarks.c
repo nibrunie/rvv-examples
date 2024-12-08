3325b83b949a50731a774187bdd23c2f57c014d6
Nicolas Brunie <nibrunie@gmail.com>
2024-12-08 09:46:24 -0800

    Extending micro-benchmark to cover a few integer instructions
    * add, div(u)(w), rem(u)(w)

--- src/ubench/microbenchmarks.c
+++ src/ubench/microbenchmarks.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <assert.h>
 #include <bench_utils.h>
+#include "ubench_utils.h"
 
 
 /** Number of executed test(s) */
@@ -11,25 +12,30 @@
 #define NUM_TESTS 10
 #endif
 
-typedef struct {
-    unsigned long perf_count;
-    int errors;
-} ubench_result_t;
-
-static ubench_result_t accumulate_ubench_result(ubench_result_t res, ubench_result_t new_result) {
-  res.perf_count += new_result.perf_count;
-  res.errors     |= new_result.errors;
-
-  return res;
-}
-
 /** Declaring various softmax implementation benchmarks **/
 ubench_result_t baseline_bench(size_t n);
+
+
 ubench_result_t baseline_bench(size_t n) {
     long start = read_perf_counter();
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n / 16; i++) {
         asm volatile(
-            "nop"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
+            "nop\n"
             :
             :
             :
@@ -43,23 +49,58 @@ ubench_result_t baseline_bench(size_t n) {
     };
 };
 
-typedef ubench_result_t (bench_func_t)(size_t);
+BENCH_0OP_INSN(nop)
+BENCH_LAT_2OP_INSN(add)
+BENCH_LAT_2OP_INSN(div)
+BENCH_LAT_2OP_INSN(divu)
+BENCH_LAT_2OP_INSN(divw)
+BENCH_LAT_2OP_INSN(divuw)
+BENCH_LAT_2OP_INSN(rem)
+BENCH_LAT_2OP_INSN(remu)
+BENCH_LAT_2OP_INSN(remw)
+BENCH_LAT_2OP_INSN(remuw)
 
-/** Descriptor structure for softmax benchmark */
-typedef struct {
-    bench_func_t* bench;
-    ubench_result_t result;
-    char label[256];
-} ubench_t;
-
+BENCH_THROUGHPUT_2OP_INSN(add)
+BENCH_THROUGHPUT_2OP_INSN(div)
+BENCH_THROUGHPUT_2OP_INSN(divu)
+BENCH_THROUGHPUT_2OP_INSN(divw)
+BENCH_THROUGHPUT_2OP_INSN(divuw)
+BENCH_THROUGHPUT_2OP_INSN(rem)
+BENCH_THROUGHPUT_2OP_INSN(remu)
+BENCH_THROUGHPUT_2OP_INSN(remw)
+BENCH_THROUGHPUT_2OP_INSN(remuw)
 
 int main(void) {
     int i;
     ubench_t benchmarks[] = {
-        (ubench_t){.bench = baseline_bench,  .label="nop benchmark"},
-    };
+        (ubench_t){.bench = baseline_bench,  .label="baseline benchmark"},
+        BENCH_LAT_INSN_TC(nop),
+        BENCH_LAT_INSN_TC(add),
+        BENCH_LAT_INSN_TC(div),
+        BENCH_LAT_INSN_TC(divu),
+        BENCH_LAT_INSN_TC(divw),
+        BENCH_LAT_INSN_TC(divuw),
+        BENCH_LAT_INSN_TC(rem),
+        BENCH_LAT_INSN_TC(remu),
+        BENCH_LAT_INSN_TC(remw),
+        BENCH_LAT_INSN_TC(remuw),
 
-    size_t testSizes[] = {127, 2048, 65536};
+        BENCH_THROUGHPUT_INSN_TC(add),
+        BENCH_THROUGHPUT_INSN_TC(div),
+        BENCH_THROUGHPUT_INSN_TC(divu),
+        BENCH_THROUGHPUT_INSN_TC(divw),
+        BENCH_THROUGHPUT_INSN_TC(divuw),
+        BENCH_THROUGHPUT_INSN_TC(rem),
+        BENCH_THROUGHPUT_INSN_TC(remu),
+        BENCH_THROUGHPUT_INSN_TC(remw),
+        BENCH_THROUGHPUT_INSN_TC(remuw),
+    };
+#ifndef VERBOSE
+    // condensed display
+    printf("label, test-size, counter-value, metric-per-element, element-per-metric, error(s)\n");
+#endif
+
+    size_t testSizes[] = {65536};
     for (size_t testId = 0; testId < sizeof(testSizes) / sizeof(size_t); testId++)
     {
         size_t n = testSizes[testId];
@@ -112,8 +153,9 @@ int main(void) {
             printf("  error(s):  %d\n", bench_result.errors);
 #           else
             // condensed display
-            printf("%s, %d, %d, %d\n", 
+            printf("%s, %d, %d, %.3f, %.2f, %d\n", 
                    benchmarks[benchId].label, n, bench_result.perf_count,
+                   (double) bench_result.perf_count / n, (double) n / bench_result.perf_count,
                    bench_result.errors);
 #           endif
         }
