137921ed08f3918fe161fd7074fef83d8d5b7944
Nicolas Brunie <nibrunie@gmail.com>
2025-04-05 14:58:37 -0700

    Prototyping FP reduction examples (dot-product using RVV)

--- src/reduction/reduction_bench.c
+++ src/reduction/reduction_bench.c
@@ -77,16 +77,121 @@ float vector_dot_product(float* lhs, float* rhs, size_t len) {
 }
 
 
+/** RVV-based vector dot product using ordered reduction sum
+ *
+ * @param lhs pointer to the left hand side vector
+ * @param rhs pointer to the right hand side vector
+ * @param len length of the vectors
+ * @return the dot product of the two vectors
+ */
+float rvv_dot_product(float* lhs, float* rhs, size_t len) {
+    float res = 0.f;
+    asm __volatile__ (
+        "vsetivli x0, 1, e32, m1\n"     // set vector length, before initializing the scalar value
+        "vfmv.v.f v24, %[res]\n"         // initializing the temporary accumulator in v24[0] to zero
+    "rvv_dot_product_loop:\n"                              // label for the loop
+        "vsetvli a4, %[avl], e32, m8\n" // set vector length
+        "vle32.v v8, (%[lhs])\n"        // load left hand side vector from memory
+        "vle32.v v16, (%[rhs])\n"        // load right hand side vector from memory
+        "vfmul.vv v8, v8, v16\n"         // multiply the two vectors element-wise
+        "vfredosum.vs v24, v8, v24\n"     // accumulate the product into the temporary accumulator (v24[0])
+        "sub %[avl], %[avl], a4\n"      // decrement the remaining length
+        "sh2add %[lhs], a4, %[lhs]\n"   // move the vector pointer forward by the number of processed elements
+        "sh2add %[rhs], a4, %[rhs]\n"   // move the vector pointer forward by the number of processed elements
+        "bnez %[avl], rvv_dot_product_loop\n"             // if there are more elements to process, loop back
+        "vfmv.f.s %[res], v24\n"         // move the final minimum value from vector register to scalar
+        : [avl]"+r"(len), [res]"+f"(res), [lhs]"+r"(lhs), [rhs]"+r"(rhs)
+        :
+        : "memory", "a4", "v8", "v16", "v24" // clobbered registers
+    );
+    return res;
+}
+
+
+/** RVV-based vector dot product using unordered reduction sum
+ *
+ * @param lhs pointer to the left hand side vector
+ * @param rhs pointer to the right hand side vector
+ * @param len length of the vectors
+ * @return the dot product of the two vectors
+ */
+float rvv_dot_product_u(float* lhs, float* rhs, size_t len) {
+    float res = 0.f;
+    asm __volatile__ (
+        "vsetivli x0, 1, e32, m1\n"     // set vector length, before initializing the scalar value
+        "vfmv.v.f v24, %[res]\n"         // initializing the temporary accumulator in v24[0] to zero
+    "rvv_dot_product_u_loop:\n"                              // label for the loop
+        "vsetvli a4, %[avl], e32, m8\n" // set vector length
+        "vle32.v v8, (%[lhs])\n"        // load left hand side vector from memory
+        "vle32.v v16, (%[rhs])\n"        // load right hand side vector from memory
+        "vfmul.vv v8, v8, v16\n"         // multiply the two vectors element-wise
+        "vfredusum.vs v24, v8, v24\n"     // accumulate the product into the temporary accumulator (v24[0])
+        "sub %[avl], %[avl], a4\n"      // decrement the remaining length
+        "sh2add %[lhs], a4, %[lhs]\n"   // move the vector pointer forward by the number of processed elements
+        "sh2add %[rhs], a4, %[rhs]\n"   // move the vector pointer forward by the number of processed elements
+        "bnez %[avl], rvv_dot_product_u_loop\n"             // if there are more elements to process, loop back
+        "vfmv.f.s %[res], v24\n"         // move the final minimum value from vector register to scalar
+        : [avl]"+r"(len), [res]"+f"(res), [lhs]"+r"(lhs), [rhs]"+r"(rhs)
+        :
+        : "memory", "a4", "v8", "v16", "v24" // clobbered registers
+    );
+    return res;
+}
+
+/** RVV-based vector dot product using element wise accumulation
+ *
+ * @param lhs pointer to the left hand side vector
+ * @param rhs pointer to the right hand side vector
+ * @param len length of the vectors
+ * @return the dot product of the two vectors
+ */
+float rvv_dot_product_2_steps(float* lhs, float* rhs, size_t len) {
+    float res = 0.f;
+    asm __volatile__ (
+        "vsetvli a4, x0, e32, m8\n" // set vector length
+        "vfmv.v.f v24, %[res]\n"    // initializing the temporary accumulators in v24 to zeros
+        "vmv.v.i v24, 0\n"
+    "rvv_dot_product_2_steps_loop:\n"                              // label for the loop
+        "vsetvli a4, %[avl], e32, m8, tu, mu\n" // set vector length
+        "vle32.v v8, (%[lhs])\n"        // load left hand side vector from memory
+        "vle32.v v16, (%[rhs])\n"        // load right hand side vector from memory
+        // could be (should be) fused into element-wise vfmadd
+        "vfmul.vv v8, v8, v16\n"         // multiply the two vectors element-wise
+        "vfadd.vv v24, v8, v24\n"     // accumulate the product into the temporary accumulator (v24[0])
+        "sub %[avl], %[avl], a4\n"      // decrement the remaining length
+        "sh2add %[lhs], a4, %[lhs]\n"   // move the vector pointer forward by the number of processed elements
+        "sh2add %[rhs], a4, %[rhs]\n"   // move the vector pointer forward by the number of processed elements
+        "bnez %[avl], rvv_dot_product_2_steps_loop\n"             // if there are more elements to process, loop back
+
+        "vsetvli a4, x0, e32, m8\n" // set vector length
+        "vmv.v.i v16, 0\n" // initialize v16 to zero for the final reduction step
+        "vfredusum.vs v16, v24, v16\n"   // this is a second step to ensure the final reduction in case of multiple vector registers
+        "vfmv.f.s %[res], v16\n"         // move the final minimum value from vector register to scalar
+        : [avl]"+r"(len), [res]"+f"(res), [lhs]"+r"(lhs), [rhs]"+r"(rhs)
+        :
+        : "memory", "a4", "v8", "v16", "v24" // clobbered registers
+    );
+    return res;
+}
+
+
 typedef struct {
     int32_t (*red_func)(int32_t* vec, size_t len);
     int32_t (*golden)(int32_t* vec, size_t len);
     char* label;
-} reduction_bench_t;
+} int_reduction_bench_t;
+
+typedef struct {
+    float (*red_func)(float* lhs, float*rhs, size_t len);
+    float (*golden)(float* lhs, float* rhs, size_t len);
+    char* label;
+} float_reduction_bench_t;
+
 
 int bench_int_reduction(void) {
     uint32_t start = 0, stop = 0;
 
-    reduction_bench_t benchmarks[] = {
+    int_reduction_bench_t benchmarks[] = {
         // labels should be padded to all have the same width (result display alignment)
         {.red_func = golden_min,      .golden = golden_min, .label = "generic vector min reduction"},
         {.red_func = rvv_min,         .golden = golden_min, .label = "RVV based vector min reduction"},
@@ -108,9 +213,9 @@ int bench_int_reduction(void) {
             inputVec[i] = (int32_t)(rand()); // fill the vector with random integers in the range [0, 100)
         }
 
-        for (int bench_id = 0; bench_id < sizeof(benchmarks) / sizeof(reduction_bench_t); bench_id++) {
+        for (int bench_id = 0; bench_id < sizeof(benchmarks) / sizeof(int_reduction_bench_t); bench_id++) {
             start = read_perf_counter();
-            uint32_t result = benchmarks[bench_id].red_func(inputVec, vecSize); // call the reduction function
+            int32_t result = benchmarks[bench_id].red_func(inputVec, vecSize); // call the reduction function
             stop = read_perf_counter();
 
             // computing golden
@@ -140,6 +245,78 @@ int bench_int_reduction(void) {
 }
 
 
+int bench_float_reduction(void) {
+    uint32_t start = 0, stop = 0;
+
+    float_reduction_bench_t benchmarks[] = {
+        // labels should be padded to all have the same width (result display alignment)
+        //{.red_func = vector_dot_product,      .golden = vector_dot_product, .label = "generic vector dot product"},
+        // {.red_func = rvv_dot_product_2_steps, .golden = vector_dot_product, .label = "RVV-based vector dot product (parallel accumulation)"},
+        //{.red_func = rvv_dot_product,         .golden = vector_dot_product, .label = "RVV-based vector dot product (ordered vfredosum)"},
+        //{.red_func = rvv_dot_product_u,       .golden = vector_dot_product, .label = "RVV-based vector dot product (unordered vfredusum)"},
+
+        {.red_func = rvv_dot_product_2_steps, .golden = rvv_dot_product, .label = "RVV-based vector dot product (parallel accumulation)"},
+    };
+
+    size_t vectorSizes[] = {MSG_SIZES}; // 16, 32, 64, 128, 511, 512, 513, 1024, 16384, MSG_SIZES};
+    int error = 0;
+#   ifndef VERBOSE
+        printf("message_size, red_result, label, perf(" PERF_METRIC ")\n");
+#   endif
+
+    for (int size_id = 0; size_id < sizeof(vectorSizes) / sizeof(size_t); ++size_id) {
+        size_t vecSize = vectorSizes[size_id];
+        float* inputVecLHS = (float*) malloc(vecSize * sizeof(float));
+        assert(inputVecLHS != NULL && "Failed to allocate memory for input vector"); // ensure allocation was successful
+        float* inputVecRHS = (float*) malloc(vecSize * sizeof(float));
+        assert(inputVecRHS != NULL && "Failed to allocate memory for input vector"); // ensure allocation was successful
+
+        for (size_t i = 0; i < vecSize; ++i) {
+            // fill the input vectors with random floats in the range [0, 17)
+            // floats are actually converted integer so we do not get rounding errors for small enough vector sizes
+            inputVecLHS[i] = (float)(i % 17); // (float)(rand() % 17);
+            inputVecRHS[i] = (float)(i % 17);
+        }
+
+        for (int bench_id = 0; bench_id < sizeof(benchmarks) / sizeof(float_reduction_bench_t); bench_id++) {
+            start = read_perf_counter();
+            float result = benchmarks[bench_id].red_func(inputVecLHS, inputVecRHS, vecSize); // call the reduction function
+            stop = read_perf_counter();
+
+            // computing golden
+            float golden = benchmarks[bench_id].golden(inputVecLHS, inputVecRHS, vecSize);
+
+            // checks
+            error += result != golden;
+
+            uint32_t delay = stop - start;
+            float throughput = (double) delay / vecSize; 
+
+            // full message
+    #       ifdef VERBOSE
+            printf("reduction(vector[%lu]) = %f (%s)      in %u " PERF_METRIC "(s) [%.3f " PERF_METRIC "(s) per Byte]\n",
+                vecSize, result, benchmarks[bench_id].label, delay, throughput);
+    #       else
+            printf("%8lu, %f, %s, %u\n", vecSize, result, benchmarks[bench_id].label, delay);
+    #       endif
+
+        }
+
+        free(inputVecLHS);
+        free(inputVecRHS);
+
+    }
+
+    return error;
+}
+
+
 int main(void) {
-    return bench_int_reduction();
+    int error = 0;
+    
+    // error |= bench_int_reduction();
+
+    error |= bench_float_reduction();
+
+    return error;
 }
