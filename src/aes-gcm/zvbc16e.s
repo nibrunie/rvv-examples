b4a2df6ee6d9f1a845722966c1e790cfed166cda
Nicolas Brunie <nibrunie@gmail.com>
2025-05-11 09:07:08 -0700

    Adding sanity tests for SEW=16 and 8 for Zvbc32e vclmul{h}.v[v,x]

---
+++ src/aes-gcm/zvbc16e.s
@@ -0,0 +1,183 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022 by Rivos Inc.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+#
+# The Zvbc32e extension contains vectorized carryless multiply (vclmul, vclmulh).
+#
+# Those routines are vector-length (VLEN) agnostic, only requiring
+# that VLEN is a multiple of 32. Smaller VLENs should work when using
+# LMUL>1, but this is not exercised here.
+#
+# This code was developed to validate the design of the Zvbc extension, and to
+# understand and demonstrate expected usage patterns.
+#
+# DISCLAIMER OF WARRANTY:
+#  This code is not intended for use in real cryptographic applications,
+#  has not been reviewed, even less audited by cryptography or security
+#  experts, etc.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER "AS IS" AND ANY EXPRESS
+#  OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+#  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+#  IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+#  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+#  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+.text
+
+######################################################################
+# Vector Carryless Multiply routines
+######################################################################
+
+# zvbc_vclmul_vv
+#
+# Takes two vectors of uint32_t elements vs2, vs1 as input,
+# a number of (32 bit) elements 'n', sets destination vector
+# to clmul(vs2, vs1)
+#
+# Returns the number of elements processed, which is 'n'.
+#
+# C Signature
+#   extern "C" uint32_t
+#   zvbc16e_vclmul_vv(
+#       uint32_t* dest,       // a0
+#       const uint32_t* vs2,  // a1
+#       const uint32_t* vs1,  // a2
+#       size_t n              // a3
+#  );
+#  a0=dest, a1=vs2, a2 = vs1, a3 = n
+#
+.balign 4
+.global zvbc16e_vclmul_vv
+zvbc16e_vclmul_vv:
+    mv t1, a3
+1:
+    vsetvli t0, a3, e16, m1, ta, ma
+    vle16.v v2, (a1)  # get vs2
+    vle16.v v1, (a2)  # get vs1
+    vclmul.vv v0, v2, v1  # vd, vs2, vs1
+    vse16.v v0, (a0)
+    sub a3, a3, t0
+    slli t0, t0, 1  # t0 <- #bytes processed in this iteration.
+    add a0, a0, t0
+    add a1, a1, t0
+    add a2, a2, t0
+    bnez a3, 1b
+
+    mv a0, t1
+    ret
+
+# zvbc16e_vclmul_vx
+#
+# Takes a vector of uint32_t elements vs2, a uint32_t scalar rs1,
+# and a number of (32 bit) elements 'n' as inputs, sets the destination
+# vector to vclmul(vs2, rs1)
+#
+# Returns the number of elements processed, which is 'n'.
+#
+# C Signature
+#   extern "C" uint32_t
+#   zvbc16e_vclmul_vv(
+#       uint32_t* dest,       // a0
+#       const uint32_t* vs2,  // a1
+#       uint32_t rs1,         // a2
+#       size_t n              // a3
+#  );
+#  a0=dest, a1=vs2, a2 = vs1, a3 = n
+#
+.balign 4
+.global zvbc16e_vclmul_vx
+zvbc16e_vclmul_vx:
+    mv t1, a3
+1:
+    vsetvli t0, a3, e16, m1, ta, ma
+    vle16.v v2, (a1) # get vs2
+    vclmul.vx v0, v2, a2  # vd, vs2, rs1
+    vse16.v v0, (a0)
+    sub a3, a3, t0
+    slli t0, t0, 1  # t0 <- #bytes processed in this iteration.
+    add a0, a0, t0
+    add a1, a1, t0
+    bnez a3, 1b
+
+    mv a0, t1
+    ret
+
+# zvbc16e_vclmulh_vv
+#
+# Takes two vectors of uint32_t elements vs2, vs1 as input,
+# a number of (32 bit) elements 'n', sets destination vector
+# to clmulh(vs2, vs1)
+#
+# Returns the number of elements processed, which is 'n'.
+#
+# C Signature
+#   extern "C" uint32_t
+#   zvbc16e_vclmulh_vv(
+#       uint32_t* dest,       // a0
+#       const uint32_t* vs2,  // a1
+#       const uint32_t* vs1,  // a2
+#       size_t n              // a3
+#  );
+#  a0=dest, a1=vs2, a2 = vs1, a3 = n
+#
+.balign 4
+.global zvbc16e_vclmulh_vv
+zvbc16e_vclmulh_vv:
+    mv t1, a3
+1:
+    vsetvli t0, a3, e16, m1, ta, ma
+    vle16.v v2, (a1)  # get vs2
+    vle16.v v1, (a2)  # get vs1
+    vclmulh.vv v0, v2, v1  # vd, vs2, vs1
+    vse16.v v0, (a0)
+    sub a3, a3, t0
+    slli t0, t0, 1  # t0 <- #bytes processed in this iteration.
+    add a0, a0, t0
+    add a1, a1, t0
+    add a2, a2, t0
+    bnez a3, 1b
+
+    mv a0, t1
+    ret
+
+# zvbc16e_vclmulh_vx
+#
+# Takes a vector of uint32_t elements vs2, a uint64_t scalar rs1,
+# and a number of (32 bit) elements 'n' as inputs, sets the destination
+# vector to vclmulh(vs2, rs1)
+#
+# Returns the number of elements processed, which is 'n'.
+#
+# C Signature
+#   extern "C" uint32_t
+#   zvbc16e_vclmulh_vv(
+#       uint32_t* dest,       // a0
+#       const uint32_t* vs2,  // a1
+#       uint32_t rs1,         // a2
+#       size_t n              // a3
+#  );
+#  a0=dest, a1=vs2, a2 = vs1, a3 = n
+#
+.balign 4
+.global zvbc16e_vclmulh_vx
+zvbc16e_vclmulh_vx:
+    mv t1, a3
+1:
+    vsetvli t0, a3, e16, m1, ta, ma
+    vle16.v v2, (a1) # get vs2
+    vclmulh.vx v0, v2, a2  # vd, vs2, rs1
+    vse16.v v0, (a0)
+    sub a3, a3, t0
+    slli t0, t0, 1  # t0 <- #bytes processed in this iteration.
+    add a0, a0, t0
+    add a1, a1, t0
+    bnez a3, 1b
+
+    mv a0, t1
+    ret
--- src/aes-gcm/zvbc32e-test.c
+++ src/aes-gcm/zvbc32e-test.c
@@ -63,6 +63,78 @@ zvbc32e_vclmulh_vx(
     uint32_t n
 );
 
+// SEW=16-bit VCLMUL
+
+extern uint32_t
+zvbc16e_vclmul_vv(
+    uint16_t* dest,
+    const uint16_t* src2,
+    const uint16_t* src1,
+    uint32_t n
+);
+
+extern uint32_t
+zvbc16e_vclmul_vx(
+    uint16_t* dest,
+    const uint16_t* src2,
+    uint16_t rs1,
+    uint32_t n
+);
+
+// SEW=16-bit VCLMULH
+
+extern uint32_t
+zvbc16e_vclmulh_vv(
+    uint16_t* dest,
+    const uint16_t* src2,
+    const uint16_t* src1,
+    uint32_t n
+);
+
+extern uint32_t
+zvbc16e_vclmulh_vx(
+    uint16_t* dest,
+    const uint16_t* src2,
+    uint16_t rs1,
+    uint32_t n
+);
+
+// SEW=8-bit VCLMUL
+
+extern uint32_t
+zvbc8e_vclmul_vv(
+    uint8_t* dest,
+    const uint8_t* src2,
+    const uint8_t* src1,
+    uint32_t n
+);
+
+extern uint32_t
+zvbc8e_vclmul_vx(
+    uint8_t* dest,
+    const uint8_t* src2,
+    uint8_t rs1,
+    uint32_t n
+);
+
+// SEW=8-bit VCLMULH
+
+extern uint32_t
+zvbc8e_vclmulh_vv(
+    uint8_t* dest,
+    const uint8_t* src2,
+    const uint8_t* src1,
+    uint32_t n
+);
+
+extern uint32_t
+zvbc8e_vclmulh_vx(
+    uint8_t* dest,
+    const uint8_t* src2,
+    uint8_t rs1,
+    uint32_t n
+);
+
 
 void
 Assert(bool predicate, int lineno, const char* filename)
@@ -78,20 +150,27 @@ Assert(bool predicate, int lineno, const char* filename)
 
 typedef __uint128_t uint128_t;
 
-// @brief Return a 32-bit randomly generated number using rand()
+// @brief Return a n-bit randomly generated number using rand()
 //
-// @return uint32_t
+// @return uint<n>_t
 //
-uint32_t
-rand32()
-{
-    return rand();
+#define RAND_N(n) \
+uint ##n##_t rand##n() { \
+    return (uint##n##_t)rand(); \
 }
 
+RAND_N(32)
+RAND_N(16)
+RAND_N(8)
+
+/** generic helper to compute a nxn carry-less multiply
+ *  assuming 1 <= n <= 32.
+ *  The full result (2n-1 bits) is returned.
+ */
 uint64_t
-carryless_multiply_32x32(uint32_t a32, uint32_t b32)
+carryless_multiply(uint32_t a32, uint32_t b32, uint32_t n)
 {
-    const size_t kInputBits = 32;
+    const size_t kInputBits = n;
     const uint64_t b64 = b32;
 
     uint64_t accumulator = 0;
@@ -105,51 +184,13 @@ carryless_multiply_32x32(uint32_t a32, uint32_t b32)
     return accumulator;
 }
 
-// @brief A helper function used to test the vclmul_vv instruction.
-//
-// This function inputs two vectors consisting of 64-bit scalars and outputs a
-// vector such that c[i] is the low 64 bits of the 64x64->128 bit carryless
-// multiplication of a[i] and b[i].
-//
-// @param a: The first vector we are multiplying
-// @param b: The second vector we are multiplying
-// @param n: Size of a, b and c vectors
-// @param c: The output vector
-//
-void
-carryless_multiply32vv_low(const uint32_t* a,
-                           const uint32_t* b,
-                           size_t n,
-                           uint32_t* c)
+uint64_t
+carryless_multiply_32x32(uint32_t a32, uint32_t b32)
 {
-    for (size_t i = 0; i < n; ++i) {
-        const uint64_t result64 = carryless_multiply_32x32(a[i], b[i]);
-        c[i] = (uint32_t)result64;
-    }
+    return carryless_multiply(a32, b32, 32);
 }
 
-// @brief A helper function used to test the vclmul_vx/vclmul_vi instructions.
-//
-// This function inputs a vectors consisting of 64-bit scalars and a scalar.
-// Outputs a vector such that c[i] is the low 64 bits of the 64x64->128 bit
-// carryless multiplication of a[i] and b.
-//
-// @param a: The first vector we are multiplying
-// @param b: The scalar we are multiplying
-// @param n: Size of a, b and c vectors
-// @param c: The output vector
-//
-void
-carryless_multiply32vx_low(const uint32_t* a,
-                           const uint32_t b,
-                           size_t n,
-                           uint32_t* c)
-{
-    for (size_t i = 0; i < n; ++i) {
-        const uint64_t result64 = carryless_multiply_32x32(a[i], b);
-        c[i] = (uint32_t)result64;
-    }
-}
+
 
 // @brief A helper function used to test the vclmul_vv instruction.
 //
@@ -163,14 +204,15 @@ carryless_multiply32vx_low(const uint32_t* a,
 // @param c: The output vector
 //
 void
-carryless_multiply32vv_high(const uint32_t* a,
+carryless_multiply32vv(const uint32_t* a,
                             const uint32_t* b,
+                            uint32_t shift,
                             size_t n,
                             uint32_t* c)
 {
     for (size_t i = 0; i < n; ++i) {
         const uint64_t result64 = carryless_multiply_32x32(a[i], b[i]);
-        c[i] = (uint32_t)(result64 >> 32);
+        c[i] = (uint32_t)(result64 >> shift);
     }
 }
 
@@ -186,16 +228,70 @@ carryless_multiply32vv_high(const uint32_t* a,
 // @param c: The output vector
 //
 void
-carryless_multiply32vx_high(const uint32_t* a,
+carryless_multiply32vx(const uint32_t* a,
                             const uint32_t b,
+                            uint32_t shift,
                             size_t n,
                             uint32_t* c)
 {
     for (size_t i = 0; i < n; ++i) {
         const uint64_t result64 = carryless_multiply_32x32(a[i], b);
-        c[i] = (uint32_t)(result64 >> 32);
+        c[i] = (uint32_t)(result64 >> shift);
     }
 }
+
+void
+carryless_multiply16vv(const uint16_t* a,
+                       const uint16_t* b,
+                       uint16_t shift,
+                       size_t n,
+                       uint16_t* c)
+{
+    for (size_t i = 0; i < n; ++i) {
+        const uint64_t result64 = carryless_multiply(a[i], b[i], 16);
+        c[i] = (uint16_t)(result64 >> shift);
+    }
+}
+
+void
+carryless_multiply16vx(const uint16_t* a,
+                       const uint16_t b,
+                       uint16_t shift,
+                       size_t n,
+                       uint16_t* c)
+{
+    for (size_t i = 0; i < n; ++i) {
+        const uint64_t result64 = carryless_multiply(a[i], b, 16);
+        c[i] = (uint16_t)(result64 >> shift);
+    }
+}
+
+void carryless_multiply8vv(const uint8_t* a,
+                       const uint8_t* b,
+                       uint8_t shift,
+                       size_t n,
+                       uint8_t* c)
+{
+    for (size_t i = 0; i < n; ++i) {
+        const uint64_t result64 = carryless_multiply(a[i], b[i], 8);
+        c[i] = (uint8_t)(result64 >> shift);
+    }
+}
+
+void carryless_multiply8vx(const uint8_t* a,
+                       const uint8_t b,
+                       uint8_t shift,
+                       size_t n,
+                       uint8_t* c)
+{
+    for (size_t i = 0; i < n; ++i) {
+        const uint64_t result64 = carryless_multiply(a[i], b, 8);
+        c[i] = (uint8_t)(result64 >> shift);
+    }
+}
+
+#define kNumElements 33
+#define kRounds 1000
 
 // @brief Tests vectorized carryless multiply intrinsic in assembly
 // using randomly generated test vectors
@@ -203,15 +299,19 @@ carryless_multiply32vx_high(const uint32_t* a,
 // @return int 0 if intrinsics worked, 1 if they failed
 //
 int
-test_rand_vclmul_32e()
+test_rand_vclmul_32e(int high)
 {
-#define kNumElements 33
-#define kRounds 1000
 
     uint32_t vs1[kNumElements];
     uint32_t vs2[kNumElements];
 
-    LOG("--- Testing Vectorized Carryless Multiply (vector vector) 32-bit elements");
+    uint32_t shift = high ? 32 : 0;
+
+    if (high) {
+        LOG("--- Testing Vectorized Carryless Multiply High (vector vector) 32-bit elements");
+    } else {
+        LOG("--- Testing Vectorized Carryless Multiply [Low] (vector vector) 32-bit elements");
+    }
 
     for (size_t round = 0; round < kRounds; ++round) {
         for (size_t i = 0; i < kNumElements; ++i) {
@@ -219,10 +319,11 @@ test_rand_vclmul_32e()
             vs2[i] = rand32();
         }
         uint32_t expected[kNumElements];
-        carryless_multiply32vv_low(vs2, vs1, kNumElements, expected);
+        carryless_multiply32vv(vs2, vs1, shift, kNumElements, expected);
 
         uint32_t actual[kNumElements] = { 0 };
         const uint32_t processed =
+          high ? zvbc32e_vclmulh_vv(actual, vs2, vs1, kNumElements) : 
           zvbc32e_vclmul_vv(actual, vs2, vs1, kNumElements);
 
         if (processed != kNumElements ||
@@ -237,17 +338,23 @@ test_rand_vclmul_32e()
         }
     }
 
-    LOG("--- Testing Vectorized Carryless Multiply (vector scalar) 32-bit elements");
+    if (high) {
+        LOG("--- Testing Vectorized Carryless Multiply High (vector scalar) 32-bit elements");
+    } else {
+        LOG("--- Testing Vectorized Carryless Multiply [Low] (vector scalar) 32-bit elements");
+    }
+
     for (size_t round = 0; round < kRounds; ++round) {
         for (size_t i = 0; i < kNumElements; ++i) {
             vs2[i] = rand32();
         }
         const uint32_t rs1 = rand32();
         uint32_t expected[kNumElements];
-        carryless_multiply32vx_low(vs2, rs1, kNumElements, expected);
+        carryless_multiply32vx(vs2, rs1, shift, kNumElements, expected);
 
         uint32_t actual[kNumElements] = { 0 };
-        const uint32_t processed =
+        const uint32_t processed = high ?
+          zvbc32e_vclmulh_vx(actual, vs2, rs1, kNumElements) :
           zvbc32e_vclmul_vx(actual, vs2, rs1, kNumElements);
 
         if (processed != kNumElements ||
@@ -269,58 +376,154 @@ test_rand_vclmul_32e()
     return 0;
 }
 
-// @brief Tests vectorized carryless multiply high intrinsic in assembly
+
+// @brief Tests vectorized carryless multiply intrinsic in assembly
 // using randomly generated test vectors
 //
 // @return int 0 if intrinsics worked, 1 if they failed
 //
 int
-test_rand_vclmulh_32e()
+test_rand_vclmul_16e(int high)
 {
-#define kNumElements 33
-#define kRounds 1000
 
-    uint32_t vs1[kNumElements];
-    uint32_t vs2[kNumElements];
+    uint16_t vs1[kNumElements];
+    uint16_t vs2[kNumElements];
 
-    LOG("--- Testing Vectorized Carryless Multiply High (vector vector) 32-bit elements");
+    uint16_t shift = high ? 16 : 0;
+
+    if (high) {
+        LOG("--- Testing Vectorized Carryless Multiply High (vector vector) 16-bit elements");
+    } else {
+        LOG("--- Testing Vectorized Carryless Multiply [Low] (vector vector) 16-bit elements");
+    }
+
     for (size_t round = 0; round < kRounds; ++round) {
         for (size_t i = 0; i < kNumElements; ++i) {
-            vs1[i] = rand32();
-            vs2[i] = rand32();
+            vs1[i] = rand16();
+            vs2[i] = rand16();
         }
-        uint32_t expected[kNumElements];
-        carryless_multiply32vv_high(vs2, vs1, kNumElements, expected);
+        uint16_t expected[kNumElements];
+        carryless_multiply16vv(vs2, vs1, shift, kNumElements, expected);
 
-        uint32_t actual[kNumElements] = { 0 };
-        const uint32_t processed =
-          zvbc32e_vclmulh_vv(actual, vs2, vs1, kNumElements);
+        uint16_t actual[kNumElements] = { 0 };
+        const uint16_t processed =
+          high ? zvbc16e_vclmulh_vv(actual, vs2, vs1, kNumElements) : 
+          zvbc16e_vclmul_vv(actual, vs2, vs1, kNumElements);
 
         if (processed != kNumElements ||
             memcmp(actual, expected, sizeof(actual))) {
             LOG("FAILURE: 'actual' does NOT match 'expected'");
             for (size_t i = 0; i < kNumElements; ++i) {
-                LOG("expected[%3zd]: 0x%08" PRIx32
-                    ", actual[%3zd]: 0x%08" PRIx32,
+                LOG("expected[%3zd]: 0x%04" PRIx16
+                    ", actual[%3zd]: 0x%04" PRIx16,
                     i, expected[i], i, actual[i]);
             }
             return 1;
         }
     }
 
-    LOG("--- Testing Vectorized Carryless Multiply High (vector scalar) 32-bit elements");
+    if (high) {
+        LOG("--- Testing Vectorized Carryless Multiply High (vector scalar) 16-bit elements");
+    } else {
+        LOG("--- Testing Vectorized Carryless Multiply [Low] (vector scalar) 16-bit elements");
+    }
+
     for (size_t round = 0; round < kRounds; ++round) {
         for (size_t i = 0; i < kNumElements; ++i) {
-            vs2[i] = rand32();
+            vs2[i] = rand16();
         }
-        const uint32_t rs1 = rand32();
+        const uint16_t rs1 = rand16();
+        uint16_t expected[kNumElements];
+        carryless_multiply16vx(vs2, rs1, shift, kNumElements, expected);
 
-        uint32_t expected[kNumElements];
-        carryless_multiply32vx_high(vs2, rs1, kNumElements, expected);
+        uint16_t actual[kNumElements] = { 0 };
+        const uint16_t processed = high ?
+          zvbc16e_vclmulh_vx(actual, vs2, rs1, kNumElements) :
+          zvbc16e_vclmul_vx(actual, vs2, rs1, kNumElements);
 
-        uint32_t actual[kNumElements] = { 0 };
-        const uint32_t processed =
-          zvbc32e_vclmulh_vx(actual, vs2, rs1, kNumElements);
+        if (processed != kNumElements ||
+            memcmp(actual, expected, sizeof(actual))) {
+            LOG("FAILURE: 'actual' does NOT match 'expected'");
+            LOG(" - round    : %zu", round);
+            LOG(" - processed: %" PRIu64, processed);
+            for (size_t i = 0; i < kNumElements; ++i) {
+                const uint16_t ac = actual[i];
+                const uint16_t ex = expected[i];
+                LOG("expected[%3zd]: 0x%04" PRIx16
+                    ", actual[%3zd]: 0x%04" PRIx16 " %s",
+                    i, ex, i, ac, (ac == ex ? "==" : "!="));
+            }
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+
+// @brief Tests vectorized carryless multiply intrinsic in assembly
+// using randomly generated test vectors
+//
+// @return int 0 if intrinsics worked, 1 if they failed
+//
+int
+test_rand_vclmul_8e(int high)
+{
+
+    uint8_t vs1[kNumElements];
+    uint8_t vs2[kNumElements];
+
+    uint8_t shift = high ? 8 : 0;
+
+    if (high) {
+        LOG("--- Testing Vectorized Carryless Multiply High (vector vector) 8-bit elements");
+    } else {
+        LOG("--- Testing Vectorized Carryless Multiply [Low] (vector vector) 8-bit elements");
+    }
+
+    for (size_t round = 0; round < kRounds; ++round) {
+        for (size_t i = 0; i < kNumElements; ++i) {
+            vs1[i] = rand8();
+            vs2[i] = rand8();
+        }
+        uint8_t expected[kNumElements];
+        carryless_multiply8vv(vs2, vs1, shift, kNumElements, expected);
+
+        uint8_t actual[kNumElements] = { 0 };
+        const uint8_t processed =
+          high ? zvbc8e_vclmulh_vv(actual, vs2, vs1, kNumElements) : 
+          zvbc8e_vclmul_vv(actual, vs2, vs1, kNumElements);
+
+        if (processed != kNumElements ||
+            memcmp(actual, expected, sizeof(actual))) {
+            LOG("FAILURE: 'actual' does NOT match 'expected'");
+            for (size_t i = 0; i < kNumElements; ++i) {
+                LOG("expected[%3zd]: 0x%02" PRIx8
+                    ", actual[%3zd]: 0x%02" PRIx8,
+                    i, expected[i], i, actual[i]);
+            }
+            return 1;
+        }
+    }
+
+    if (high) {
+        LOG("--- Testing Vectorized Carryless Multiply High (vector scalar) 8-bit elements");
+    } else {
+        LOG("--- Testing Vectorized Carryless Multiply [Low] (vector scalar) 8-bit elements");
+    }
+
+    for (size_t round = 0; round < kRounds; ++round) {
+        for (size_t i = 0; i < kNumElements; ++i) {
+            vs2[i] = rand8();
+        }
+        const uint32_t rs1 = rand8();
+        uint8_t expected[kNumElements];
+        carryless_multiply8vx(vs2, rs1, shift, kNumElements, expected);
+
+        uint8_t actual[kNumElements] = { 0 };
+        const uint32_t processed = high ?
+          zvbc8e_vclmulh_vx(actual, vs2, rs1, kNumElements) :
+          zvbc8e_vclmul_vx(actual, vs2, rs1, kNumElements);
 
         if (processed != kNumElements ||
             memcmp(actual, expected, sizeof(actual))) {
@@ -330,8 +533,8 @@ test_rand_vclmulh_32e()
             for (size_t i = 0; i < kNumElements; ++i) {
                 const uint32_t ac = actual[i];
                 const uint32_t ex = expected[i];
-                LOG("expected[%3zd]: 0x%08" PRIx32
-                    ", actual[%3zd]: 0x%08" PRIx32 " %s",
+                LOG("expected[%3zd]: 0x%02" PRIx8
+                    ", actual[%3zd]: 0x%02" PRIx8 " %s",
                     i, ex, i, ac, (ac == ex ? "==" : "!="));
             }
             return 1;
@@ -340,6 +543,8 @@ test_rand_vclmulh_32e()
 
     return 0;
 }
+
+
 
 // @brief Calls test functions for our intrinsics
 //
@@ -353,12 +558,38 @@ main()
 
     int res = 0;
 
-    res = test_rand_vclmul_32e();
+    // vclmul SEW=32-bit
+    res = test_rand_vclmul_32e(0 /* high=0 => low*/);
     if (res != 0) {
         return res;
     }
 
-    res = test_rand_vclmulh_32e();
+    // vclmulh SEW=32-bit
+    res = test_rand_vclmul_32e(1 /* high */);
+    if (res != 0) {
+        return res;
+    }
+
+    // vclmul SEW=16-bit
+    res = test_rand_vclmul_16e(0 /* high=0 => low*/);
+    if (res != 0) {
+        return res;
+    }
+
+    // vclmulh SEW=16-bit
+    res = test_rand_vclmul_16e(1 /* high */);
+    if (res != 0) {
+        return res;
+    }
+
+    // vclmul SEW=8-bit
+    res = test_rand_vclmul_8e(0 /* high=0 => low*/);
+    if (res != 0) {
+        return res;
+    }
+
+    // vclmulh SEW=8-bit
+    res = test_rand_vclmul_8e(1 /* high */);
     if (res != 0) {
         return res;
     }
